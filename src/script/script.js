9fc357606be19ed17e03beb1df4e60aa6775f0c2
marc5950 <marcuswagner5950@gmail.com>
2025-09-08 21:20:50 +0200

    fixed hero svg animation

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,26 @@
+// JS til SVG animation
 var path = document.querySelector(".hero-path path");
 var length = path.getTotalLength();
 console.log(length);
 
 path.style.strokeDasharray = length / 2;
 path.style.strokeDashoffset = length;
+
+const element = document.querySelector(".hero-path");
+
+function checkSize() {
+  const height = window.innerHeight;
+  const width = window.innerWidth;
+
+  if (height < 700 || width < 700) {
+    element.classList.add("hidden");
+  } else {
+    element.classList.remove("hidden");
+  }
+}
+
+// Kør ved load
+checkSize();
+
+// Kør igen når vinduet ændrer størrelse
+window.addEventListener("resize", checkSize);
